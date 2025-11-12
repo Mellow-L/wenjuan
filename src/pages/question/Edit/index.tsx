d47935698a004fbcb620c7c7279f8c315398a46a
Mellow-L <1245991813@qq.com>
2025-11-12 13:26:57 +0800

    完善路由，改变favicon

--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -1,7 +1,8 @@
 import React,{type FC} from "react";
 import { useParams } from "react-router-dom";
-
+import { useTitle } from "ahooks";
 const Edit: FC = () => {
+  useTitle('问卷编辑器')
   const { id = '' } = useParams()
   return (<>
     <p>Edit id为：{id} 的问卷</p>
--- src/pages/question/Stat/index.tsx
+++ src/pages/question/Stat/index.tsx
@@ -1,7 +1,7 @@
 import React,{type FC} from "react";
-
+import { useTitle } from "ahooks";
 const Stat: FC = () => {
-
+  useTitle('问卷分析器')
   return (<>
     <p>Stat</p>
   </>)
