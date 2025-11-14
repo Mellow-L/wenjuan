39497ac51d665929c7851acd9717153b28948884
林怡然 <15705643+Mellow-L@user.noreply.gitee.com>
2025-11-14 17:02:25 +0800

    完善 404 页

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,22 @@
 import React,{type FC} from "react";
-
+import { Button, Result, Space } from 'antd';
+import { useNavigate } from "react-router-dom";
+import { MANAGE_LIST_PATHNAME } from "../router";
 const NotFound: FC = () => {
-
+  const nav = useNavigate()
   return (<>
-    <p>NotFound</p>
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，您访问的页面不存在"
+      extra={
+        <Space>
+          <Button type="primary" onClick={()=>nav(-1)}>返回</Button>
+          <Button type="primary" onClick={()=>nav(MANAGE_LIST_PATHNAME)}>去问卷列表</Button>
+        </Space>      
+      }
+    />
   </>)
 }
 
-export default NotFound
+export default NotFound 
