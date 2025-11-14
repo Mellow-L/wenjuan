542282da244480ddea3c4cd395ea33088266f5c5
林怡然 <15705643+Mellow-L@user.noreply.gitee.com>
2025-11-14 15:17:28 +0800

    增加Navbar的 logo 和 userInfo

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -4,6 +4,7 @@ import styles from "./ManageLayout.module.scss";
 // import { Link } from "react-router-dom";
 import {Button, Space} from 'antd'
 import {PlusSquareOutlined,UnorderedListOutlined,StarOutlined,DeleteOutlined} from '@ant-design/icons'
+import { MANAGE_LIST_PATHNAME, MANAGE_STAR_PATHNAME, MANAGE_TRASH_PATHNAME } from "../router";
 
 const ManageLayout: FC = () => {
 	const nav = useNavigate()
@@ -13,13 +14,13 @@ const ManageLayout: FC = () => {
 			<div className={styles.left}>
         <Space direction="vertical">
 					<Button type='primary' size="large" icon={<PlusSquareOutlined />}>创建问卷</Button>
-					<Button type={pathname.startsWith('/manage/list') ? 'default':'text'} icon={<UnorderedListOutlined />} onClick={()=>nav('/manage/list')}>
+					<Button type={pathname.startsWith(MANAGE_LIST_PATHNAME) ? 'default':'text'} icon={<UnorderedListOutlined />} onClick={()=>nav(MANAGE_LIST_PATHNAME)}>
 					我的问卷列表
 					</Button>
-					<Button type={pathname.startsWith('/manage/star') ? 'default':'text'} icon={<StarOutlined />} onClick={()=>nav('/manage/star')}>
+					<Button type={pathname.startsWith(MANAGE_STAR_PATHNAME) ? 'default':'text'} icon={<StarOutlined />} onClick={()=>nav(MANAGE_STAR_PATHNAME)}>
 					我收藏的问卷
 					</Button>
-					<Button type={pathname.startsWith('/manage/trash') ? 'default':'text'} icon={<DeleteOutlined />} onClick={()=>nav('/manage/trash')}>
+					<Button type={pathname.startsWith(MANAGE_TRASH_PATHNAME) ? 'default':'text'} icon={<DeleteOutlined />} onClick={()=>nav(MANAGE_TRASH_PATHNAME)}>
 					问卷回收站
 					</Button>
 				</Space>
