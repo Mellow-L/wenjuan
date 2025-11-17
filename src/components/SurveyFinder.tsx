6ca48776aaf1b697edbf4743145a15867b536854
Mellow-L <1245991813@qq.com>
2025-11-17 10:14:53 +0800

    封装问卷搜索组件

--- src/components/SurveyFinder.tsx
+++ src/components/SurveyFinder.tsx
@@ -1,9 +1,45 @@
-import React,{type FC} from 'react'
+import { Input } from "antd";
+import React, { useEffect, useState, type ChangeEvent, type FC } from "react";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
+import { LIST_SEARCH_PARAM_KEY } from "../constant";
 
-const SurveyFinder:FC = () => {
-  return (
-    <div>SurveyFinder</div>
-  )
-}
+const SurveyFinder: FC = () => {
+	const { Search } = Input;
+	const [surveyInfo, setSurveyInfo] = useState<string>("");
+  const nav = useNavigate()
+  const {pathname} = useLocation()
+  const [searchParams] = useSearchParams()
+  useEffect(()=>{
+    const curParam = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
+    setSurveyInfo(curParam)
+  },[searchParams])
 
-export default SurveyFinder
+	function onChange(e: ChangeEvent<HTMLInputElement>) {
+		setSurveyInfo(e.target.value);
+	}
+
+	function onSearch(value: string) {
+		console.log("搜索", value);
+    // nav('/manage/list?keyword=')
+    nav({
+      pathname:pathname,
+      search:`${LIST_SEARCH_PARAM_KEY}=${value}`
+    })
+	}
+	return (
+		<> 
+			<Search
+        value={surveyInfo}
+				size="large"
+				allowClear
+				placeholder="在此输入目标问卷信息"
+				onChange={onChange}
+				onSearch={onSearch}
+				enterButton
+			/>
+			{surveyInfo}
+		</>
+	);
+};
+
+export default SurveyFinder;
