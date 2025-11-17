4ea8b00048b7d2a16e7bb4b218c45800fc86916d
林怡然 <15705643+Mellow-L@user.noreply.gitee.com>
2025-11-17 15:20:56 +0800

    完成登录注册页表单校验

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,126 @@
-import React,{type FC} from "react";
-import { useNavigate } from "react-router-dom";
-const Login: FC = () => {
-  const nav = useNavigate()
-  return (<>
-    <p>Login</p>
-    <button onClick={()=>nav(-1)}>返回</button>
-  </>)
+import { useTitle } from "ahooks";
+import React, { useEffect, type FC } from "react";
+import { Link } from "react-router-dom";
+import {
+	Button,
+	Checkbox,
+	Form,
+	Input,
+	Space,
+	Typography,
+	type FormProps,
+} from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import styles from "../styles/Common.module.scss";
+import { REGISTER_PATHNAME } from "../router";
+const { Title } = Typography;
+
+const USERNAME_KEY = "USERNAME";
+const PASSWORD_KEY = "PASSWORD";
+
+type FieldType = {
+	username: string;
+	password: string;
+	remember: boolean;
+};
+
+function storeUser(username: string, password: string) {
+	localStorage.setItem(USERNAME_KEY, username);
+	localStorage.setItem(PASSWORD_KEY, password);
 }
 
-export default Login
+function clearStoredUser() {
+	localStorage.removeItem(USERNAME_KEY);
+	localStorage.removeItem(PASSWORD_KEY);
+}
+
+function getUser() {
+	return {
+		username: localStorage.getItem(USERNAME_KEY),
+		password: localStorage.getItem(PASSWORD_KEY),
+	};
+}
+
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+	console.log("Success:", values);
+	if (values.remember) {
+		console.log("记住账号密码");
+		storeUser(values.username, values.password);
+	} else {
+		clearStoredUser();
+	}
+};
+
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+	console.log("Failed:", errorInfo);
+};
+
+const Login: FC = () => {
+	const [form] = Form.useForm();
+	useEffect(() => {
+		const storedInfo = getUser();
+		form.setFieldsValue(storedInfo);
+	});
+	useTitle("登录");
+
+	return (
+		<div className={styles.container}>
+			<Space>
+				<Title level={1}>
+					<UserOutlined />
+				</Title>
+				<Title level={1}>登录</Title>
+			</Space>
+			<Form
+				name="basic"
+				labelCol={{ span: 8 }}
+				wrapperCol={{ span: 16 }}
+				style={{ maxWidth: 600 }}
+				initialValues={{ remember: false }}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
+				autoComplete="off"
+				form={form}
+			>
+				<Form.Item<FieldType>
+					label="用户名"
+					name="username"
+					rules={[
+            { required: true, message: "请输入用户名" },
+            { type:'string', min: 8, max:15,message:'用户名长度在 8～15 之间！'},
+            { pattern:/^\w+$/,message:'只能包含字母数字下划线'}
+          ]}
+				>
+					<Input />
+				</Form.Item>
+
+				<Form.Item<FieldType>
+					label="密码"
+					name="password"
+					rules={[{ required: true, message: "请输入密码" }]}
+				>
+					<Input.Password />
+				</Form.Item>
+
+				<Form.Item<FieldType>
+					name="remember"
+					valuePropName="checked"
+					label={null}
+				>
+					<Checkbox>记住密码</Checkbox>
+				</Form.Item>
+
+				<Form.Item label={null}>
+					<Space>
+						<Button type="primary" htmlType="submit">
+							提交登录
+						</Button>
+						<Link to={REGISTER_PATHNAME}>没有账号？前去注册</Link>
+					</Space>
+				</Form.Item>
+			</Form>
+		</div>
+	);
+};
+
+export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,98 @@
 import React,{type FC} from "react";
-import { useNavigate,Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Button, Form, Input, Space, Typography, type FormProps } from "antd";
+import { useTitle } from "ahooks";
+import { UserAddOutlined } from "@ant-design/icons";
+import styles from '../styles/Common.module.scss'
+import { LOGIN_PATHNAME } from "../router";
+const {Title} = Typography
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  confirmPassword?:string;
+};
+
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+  console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+  console.log('Failed:', errorInfo);
+};
+
 const Register: FC = () => {
-  const nav = useNavigate()
-  return (<>
-    <p>Register</p>
-    <button onClick={()=>nav}>返回</button>
-    <Link to="/register">注册</Link>
-  </>)
+  useTitle('注册') 
+
+  return (<div className={styles.container}>
+    <Space>
+      <Title level={1}><UserAddOutlined /></Title>
+      <Title level={1}>注册新用户</Title>
+    </Space>
+    
+    <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item<FieldType>
+        label="用户名"
+        name="username"
+        rules={[
+          { required: true, message: "请输入用户名" },
+          { type:'string', min: 8, max:15,message:'用户名长度在 8～15 之间！'},
+          { pattern:/^\w+$/,message:'只能包含字母数字下划线'}
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        label="密码"
+        name="password"
+        rules={[
+          { required: true, message: '请输入密码' },
+          { type:'string',min:8,max:15,message:'密码长度在 8～15 之间！'},
+          { pattern:/^\w+$/,message:'只能包含字母数字下划线'}
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item<FieldType>
+        label="确认密码"
+        name="confirmPassword"
+        dependencies={['password']}
+        rules={[
+          { required: true, message:'请再次输入密码'},
+          ({getFieldValue})=>({
+            validator(_,value){
+              if(!value || value === getFieldValue('password')){
+                return Promise.resolve()
+              }else{
+                return Promise.reject(new Error('前后密码不一致'))
+              }
+            }
+          })
+        ]}
+       >
+        <Input.Password/>
+      </Form.Item> 
+
+      <Form.Item label={null}>
+        <Space>
+          <Button type="primary" htmlType="submit">
+            提交注册
+          </Button>
+          <Link to={LOGIN_PATHNAME}>已有账号？前去登录</Link>
+        </Space> 
+      </Form.Item>
+    </Form>
+  </div>)
 }
 
 export default Register
