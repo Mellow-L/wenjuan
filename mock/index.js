1ce3c2f153d2b52de3c2ffa2dcb57331cedaa520
林怡然 <15705643+Mellow-L@user.noreply.gitee.com>
2025-11-22 11:30:03 +0800

    Initial commit（本地开发至删除功能）

---
+++ mock/index.js
@@ -0,0 +1,10 @@
+const test = require('./test')
+const survey = require('./survey')
+
+// mock配置列表
+const mockList = [
+  ...test,
+  ...survey
+]
+
+module.exports = mockList
---
+++ mock/survey.js
@@ -0,0 +1,58 @@
+const Mock = require('mockjs')
+const getSurveyList = require('./data/getSurveyList')
+const Random = Mock.Random
+module.exports = [
+  {
+    url:'/api/survey/:id',// 获取单个问卷信息
+    method:'get',
+    response(){
+      return {
+        errno:0,
+        data:{
+          id: Random.id(),
+          title: Random.ctitle()
+        } 
+        // errno:400,
+        // msg:' 用户未登录'
+      }
+    }
+  },
+  {
+    url:'/api/survey',// 新建问卷
+    method:'post',
+    response(){
+      return {
+        errno:0,
+        data:{
+          id:Random.id() 
+        }
+      }
+    }
+  },
+  {
+    url:'/api/survey',// 查询问卷
+    method:'get',
+    response(ctx){
+      const { url = ''} = ctx
+      const isDeleted = url.indexOf('isDeleted=true') >= 0
+      const isStar = url.indexOf('isStar=true') >= 0
+      const opt = {isDeleted,isStar}
+      return {
+        errno:0,
+        data:{
+          list:getSurveyList(opt),
+          total:100 // 用于前端分页组件处理
+        }
+      }
+    }
+  },
+  {
+    url:'/api/survey', // 批量删除
+    method:'delete',
+    response(){
+      return{
+        errno: 0,
+      }
+    }
+  }
+]
