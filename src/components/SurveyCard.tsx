89497eb46518737000e78e30eaf4992c7c9bb7a9
Mellow-L <1245991813@qq.com>
2025-11-24 12:14:37 +0800

    feat：收藏问卷

--- src/components/SurveyCard.tsx
+++ src/components/SurveyCard.tsx
@@ -1,6 +1,14 @@
-import React, { type FC } from "react";
+import React, { useState, type FC } from "react";
 import styles from "./SurveyCard.module.scss";
-import { Button, Divider, message, Popconfirm, Space, Tag, type PopconfirmProps } from "antd";
+import {
+	Button,
+	Divider,
+	message,
+	Popconfirm,
+	Space,
+	Tag,
+	type PopconfirmProps,
+} from "antd";
 import {
 	CopyOutlined,
 	DeleteOutlined,
@@ -11,7 +19,8 @@ import {
 } from "@ant-design/icons";
 import { Link, useNavigate } from "react-router-dom";
 import { EDIT_PATHNAME, STAT_PATHNAME } from "../router";
-
+import { useRequest } from "ahooks";
+import { updateSurveyService } from "../services/survey";
 
 type PropsType = {
 	_id: string;
@@ -24,37 +33,52 @@ type PropsType = {
 const SurveyCard: FC<PropsType> = (props: PropsType) => {
 	const nav = useNavigate();
 	const { _id, title, isPublished, isStar, answerCount, createdAt } = props;
-	
-	const confirmDelete: PopconfirmProps['onConfirm'] = (e) => {
+
+	const confirmDelete: PopconfirmProps["onConfirm"] = (e) => {
 		console.log(e);
-		message.success('已移入回收站');
+		message.success("已移入回收站");
 		// alert('yes')
 	};
 
-	const cancelDelete: PopconfirmProps['onCancel'] = (e) => {
+	const cancelDelete: PopconfirmProps["onCancel"] = (e) => {
 		console.log(e);
-		message.error('删除失败');
+		message.error("删除失败");
 		// alert('no')
 	};
+	const [isStarState, setIsStarState] = useState(isStar);
+
+	const { loading: toggleStarLoading, run: toggleStar } = useRequest(
+		async () => {
+			console.log("toggle star");
+			await updateSurveyService(_id, { isStar: !isStarState }); 
+		}
+		,{
+			manual:true, //总是和解构出 run 伴随
+			onSuccess(){
+				setIsStarState(!isStarState)
+				message.success('收藏状态已更新')
+			}  
+		}
+	);
 
 	return (
 		<>
 			<div className={styles.container}>
 				<div className={styles.title}>
 					<div className={styles.left}>
-						<Link to={isPublished? `/survey/stat/${_id}`:`/survey/edit/${_id}` }>{title}</Link>
+						<Link
+							to={isPublished ? `/survey/stat/${_id}` : `/survey/edit/${_id}`}
+						>
+							{title}
+						</Link>
 					</div>
 					<div className={styles.right}>
-						{isPublished ? (
-              <Tag color="green">已发布</Tag>
-						) : (
-							<Tag>未发布</Tag>
-						)}
+						{isPublished ? <Tag color="green">已发布</Tag> : <Tag>未发布</Tag>}
 						<span>答卷数：{answerCount} </span>
 						<span>{createdAt}</span>
 					</div>
 				</div>
-        <Divider size="small"></Divider>
+				<Divider size="small"></Divider>
 				<Space direction="horizontal" className={styles["button-container"]}>
 					<Space className={styles.left}>
 						<Button
@@ -73,10 +97,13 @@ const SurveyCard: FC<PropsType> = (props: PropsType) => {
 						</Button>
 					</Space>
 					<Space className={styles.right}>
-						
-						<Button icon={isStar ? <StarFilled /> : <StarOutlined />}></Button>	
+						<Button
+							icon={isStarState ? <StarFilled /> : <StarOutlined />}
+							onClick={toggleStar}
+							disabled={toggleStarLoading}
+						></Button>
 
-						<Button icon={<CopyOutlined />} ></Button>						
+						<Button icon={<CopyOutlined />}></Button>
 
 						<Popconfirm
 							title="删除"
@@ -85,7 +112,7 @@ const SurveyCard: FC<PropsType> = (props: PropsType) => {
 							onCancel={cancelDelete}
 							okText="确认"
 							cancelText="取消"
-						>	
+						>
 							<Button icon={<DeleteOutlined />}></Button>
 						</Popconfirm>
 					</Space>
