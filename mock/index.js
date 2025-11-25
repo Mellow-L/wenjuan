419da2f881c2eca250b9a00e262720313a98dff1
林怡然 <15705643+Mellow-L@user.noreply.gitee.com>
2025-11-25 21:00:43 +0800

    feat：用户相关api

--- mock/index.js
+++ mock/index.js
@@ -1,10 +1,11 @@
 const test = require('./test')
 const survey = require('./survey')
-
+const user = require('./user')
 // mock配置列表
 const mockList = [
   ...test,
-  ...survey
+  ...survey,
+  ...user
 ]
 
 module.exports = mockList
---
+++ mock/user.js
@@ -0,0 +1,39 @@
+const Mock = require('mockjs')
+const Random = Mock.Random
+
+module.exports = [
+  {
+    url:'/api/user/info', // 获取用户信息
+    method:'get',
+    response(){
+      return {
+        errno:0,
+        data:{
+          username:Random.title(),
+          nickname:Random.cname()
+        }
+      }
+    }
+  },
+  {
+    url:'/api/user/register', // 注册用户
+    method:'post',
+    response(){
+      return {
+        errno:0
+      }
+    }
+  },
+  {
+    url:'/api/user/login', // 登录用户
+    method:'post',
+    response(){
+      return {
+        errno:0,
+        data:{
+          token:Random.word(20)
+        }
+      }
+    }
+  }
+]
