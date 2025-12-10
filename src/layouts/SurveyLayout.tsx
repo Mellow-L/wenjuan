aa600044449b9f3b207b0ec6cfe89592c9982e58
Mellow-L <1245991813@qq.com>
2025-12-10 14:50:44 +0800

    feat：编辑页布局

--- src/layouts/SurveyLayout.tsx
+++ src/layouts/SurveyLayout.tsx
@@ -4,11 +4,13 @@ import useLoadUserData from "../hooks/useLoadUserData";
 import { Spin } from "antd";
 import useAuthGuard from "../hooks/useAuthGuard";
 const SurveyLayout: FC = () => {
+  // 加载状态
   const {waitingUserData} = useLoadUserData()
+  // 路由守卫
   useAuthGuard(waitingUserData)
   return (<>
     <div>SurveyLayout</div>
-    <div>
+    <div style={{ height:'100vh'}}>
       {waitingUserData ? 
       <Spin tip="Loading" size="large">
         <div style={{ minHeight: 100 }} />
--- src/pages/survey/Edit/index.tsx
+++ src/pages/survey/Edit/index.tsx
@@ -2,24 +2,39 @@ import React, { type FC } from "react";
 import { useTitle } from "ahooks";
 import { Spin } from "antd";
 import useLoadSurveyData from "../../../hooks/useLoadSurveyData";
-
+import styles from './index.module.scss'
 const Edit: FC = () => {
 	useTitle("问卷编辑器");
 	const {loading,data} = useLoadSurveyData()
-	return (
-		<>
-			{/* <p>Edit id为：{id} 的问卷</p> */}
 
-			{loading ? (
-				<Spin tip="Loading" size="large">
-					<div style={{ minHeight: 100 }} />
-				</Spin> 
-			) : (
-				<p>{JSON.stringify(data)}</p>
-			)}
+	return (<div className={styles.container}>
+		<div style={{backgroundColor:'#ffffff', height:'50px'}}>header</div>
+		<div className={styles['content-wrapper']}>
+			<div className={styles.content}>
+				<div className={styles.left}>left</div>
+				<div className={styles.main}>
+					<div className={styles['canvas-wrapper']}>
+						<div style={{height:'900px',  }}>滚动画布（测试）</div>
+					</div>  
+				</div> 
+				<div className={styles.right}>right</div>
+			</div>  
+		</div> 
+	</div>)
+	// return (
+	// 	<>
+	// 		{/* <p>Edit id为：{id} 的问卷</p> */}
 
-		</>
-	);
+	// 		{loading ? (
+	// 			<Spin tip="Loading" size="large">
+	// 				<div style={{ minHeight: 100 }} />
+	// 			</Spin> 
+	// 		) : (
+	// 			<p>{JSON.stringify(data)}</p>
+	// 		)}
+
+	// 	</>
+	// );
 };
 
 export default Edit;
