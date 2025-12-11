c39a73b9877e59e32502f8dd35a1d97ecbccbbb5
林怡然 <15705643+Mellow-L@user.noreply.gitee.com>
2025-12-11 11:30:12 +0800

    fix：对齐前后端数据约定

--- mock/survey.js
+++ mock/survey.js
@@ -3,14 +3,44 @@ const getSurveyList = require('./data/getSurveyList')
 const Random = Mock.Random
 module.exports = [
   {
-    url:'/api/survey/:id',// 获取某份问卷信息
+    url:'/api/survey/:id',// 获取某单份问卷信息
     method:'get',
     response(){
       return {
         errno:0,
         data:{
           id: Random.id(),
-          title: Random.ctitle()
+          title: Random.ctitle(),
+          componentsList:[
+            {
+              fe_id:Random.id(), // 组件唯一标识 用作前端的列表渲染
+              type:'SurveyTitle', // 组件类型，前后端约定 type
+              title:'标题', // 组件名称用于左右侧栏显示
+              props:{ // 组件具体属性
+                text:'个人信息收集',
+                level:1,
+                isCenter:true
+              }
+            },
+            {
+              fe_id:Random.id(),
+              type:'SurveyInput',
+              title:'输入框',
+              props:{
+                title:'姓名',
+                placeholder:'请输入你的姓名' 
+              }
+            },
+            {
+              fe_id:Random.id(),
+              type:'SurveyInput',
+              title:'输入框2',
+              props:{
+                title:'手机号',
+                placeholder:'请输入你的手机号' 
+              }
+            },
+          ]
         } 
         // errno:400,
         // msg:' 用户未登录'
