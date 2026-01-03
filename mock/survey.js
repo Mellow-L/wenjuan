7a2fed787a626d49fa40ba1e801229a6dd3796df
Mellow-L <1245991813@qq.com>
2026-01-03 18:52:46 +0800

    问卷信息新增字段

--- mock/survey.js
+++ mock/survey.js
@@ -11,6 +11,9 @@ module.exports = [
         data:{
           id: Random.id(),
           title: Random.ctitle(),
+          desc:'描述xyz',
+          js:'hello im js',
+          css:'hi im css',
           componentsList:[
             {
               fe_id:Random.id(), // 组件唯一标识 用作前端的列表渲染
@@ -20,16 +23,86 @@ module.exports = [
                 text:'个人信息收集',
                 level:1,
                 isCenter:true
-              }
+              },
+              isHidden:false,
+              isLocked:false,
+            },
+            {
+              fe_id:Random.id(),
+              type:'SurveyParagraph', 
+              title:'段落 n', 
+              props:{ 
+                text:'一个后端返回的段落',
+                isCenter:false
+              },
+              isHidden:false,
+              isLocked:false,
+            },
+            {
+              fe_id:Random.id(),
+              type:'SurveyInfo', 
+              title:'问卷信息1', 
+              props:{ 
+                title:'问卷信息 1 的标题',
+                description:'这里描述了问卷信息 1'
+              },
+              isHidden:false,
+              isLocked:false,
+            },
+            {
+              fe_id:Random.id(),
+              type:'SurveySelectRadio', 
+              title:'单选 1', 
+              props:{ 
+                title:'单选 1的题干',
+                isVertical:true,
+                options:[
+                  {value:'default item1',label:'选项  m1'},
+                  {value:'default item2',label:'选项s 2'},
+                  {value:'default item3',label:'选项 3s'},
+                ],
+                value:'default item1'
+              },
+              isHidden:false,
+              isLocked:false,
+            },
+            {
+              fe_id:Random.id(),
+              type:'SurveySelectCheckbox', 
+              title:'多选 1', 
+              props:{ 
+                title:'多选 1 的题干',
+                isVertical:true, 
+                options:[
+                  {value:'default item1',label:'选项 a1',checked:true}, 
+                  {value:'default item2',label:'选项 s2',checked:false},
+                  {value:'default item3',label:'选项 v3',checked:true},
+                ]
+              },
+              isHidden:false,
+              isLocked:false,
             },
             {
               fe_id:Random.id(),
               type:'SurveyInput',
-              title:'输入框',
+              title:'输入框0',
               props:{
                 title:'姓名',
                 placeholder:'请输入你的姓名' 
-              }
+              },
+              isHidden:false,
+              isLocked:false,
+            },
+            {
+              fe_id:Random.id(),
+              type:'SurveyInput',
+              title:'输入框1',
+              props:{
+                title:'姓名nnn',
+                placeholder:'请输入你的姓名nnn' 
+              },
+              isHidden:false,
+              isLocked:true,
             },
             {
               fe_id:Random.id(),
@@ -38,7 +111,9 @@ module.exports = [
               props:{
                 title:'手机号',
                 placeholder:'请输入你的手机号' 
-              }
+              },
+              isHidden:false,
+              isLocked:false,
             },
           ]
         } 
