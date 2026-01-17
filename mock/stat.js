644622885c5b9724b0d9a2f42f0e2510acc583a3
Mellow-L <1245991813@qq.com>
2026-01-17 15:22:01 +0800

    接上条，答卷数据

--- mock/stat.js
+++ mock/stat.js
@@ -6,12 +6,33 @@ module.exports = [
   {
     url:'/api/stat/:surveyId',
     method:'get',
+    response(ctx){
+      const { query= {} } = ctx
+      return {
+        errno:0,
+        data:{
+          total:1000,
+          list:getStatList(query.pageSize||null)
+        }
+      }
+    }
+  },
+  {
+    url:'/api/stat/:surveyId/:componentId', // 获取某问卷单个组件的答卷可统计数据
+    method:'get',
     response(){
       return {
         errno:0,
         data:{
-          total:100,
-          list:getStatList()
+          stat:[
+            {name:'item 1',count:5},
+            {name:'item 2',count:10},
+            {name:'item 3',count:6},
+            {name:'item 4',count:21},
+            {name:'item 5',count:14},
+            {name:'item 6',count:9},
+            {name:'item 7',count:30},
+          ]
         }
       }
     }
